d2e2f63e3a4bdb427ee4951ef91a57fbf220e3a0
Scott Ammon <scott.o.ammon@gmail.com>
2018-05-28 21:23:53 -0700

    first attempt at improving computer player

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 // Global Variables
-var gameOver = false;
-var aiTest = false;
-var moveCount = 1;
-var gameBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-var currentPlayer = 'X';
+var gameOver = false; // boolean to stop game
+var aiTest = false;   // lets checkForWin know whether this is a computer move
+var moveValue = 0;    // evaluates how good a potential move is for the computer
+var moveCount = 1;    // which move the game is on
+var gameBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9]; // id's of the game board elements
+var currentPlayer = 'X'; // used to place text in clicked square
 
 var resetGame = function() {
   gameOver = false;
@@ -22,23 +23,39 @@ var resetGame = function() {
 };
 
 var checkForWin = function(squareInPlay) {
+  moveValue = 0;
   // get classes associated with square that was clicked
   var classesInPlay = squareInPlay.classList;
 
   for (let i = 0; i < classesInPlay.length; i++) {
     var winVar = 0;
+    var aiVar = 0;
     // get all board elements that have the i'th class of the clicked square
     var classesOnBoard = document.getElementsByClassName(classesInPlay[i]);
 
     // iterate over board elements to see if they have the matching letter in them
     for(let i = 0; i < classesOnBoard.length; i++) {
-      if(classesOnBoard[i].textContent === currentPlayer) {
-        winVar++;
+      if(aiTest) {
+        if(classesOnBoard[i].textContent === 'X') {
+          winVar++;
+        }
+        if(classesOnBoard[i].textContent === 'O') {
+          aiVar++;
+        }
+      } else {
+        if(classesOnBoard[i].textContent === currentPlayer) {
+          winVar++;
+        }
       }
     }
-
-    // if all three boxes of a given class have same letter in them, it's a win!
-    if(winVar === 3 && !aiTest) {
+    console.log(aiVar);
+    // if this square has more 'O's adjacent, save it
+    if(winVar > moveValue) {
+      moveValue = winVar;
+    }
+  
+    // if all three boxes of a given class have same letter in them, it's a win
+    if(winVar === 3 || aiVar === 3) {
       gameOver = true;
       // highlight the squares that won the game
       for(let i = 0; i < classesOnBoard.length; i++) {
@@ -65,28 +82,44 @@ var checkForWin = function(squareInPlay) {
 };
 
 var aiMove  = function() {
-  // get array of unplayed squares
   var gameSquares = document.getElementById('game-board').children;
-  var unplayedIndices = [];
+  var unplayedIndices = []; // store indices of the unplayed squares on the board
+  var aiSquare; // represents square that the computer will select to place 'O'
+  var moveValueArray = []; // array storing the potential value of each move
 
   for(let i = 0; i < gameSquares.length; i++) {
     // if the square is unplayed
     if(gameSquares[i].getAttribute('data-id') === 'not-played') {
       unplayedIndices.push(i);
+      aiTest = true;
     }
   }
-  // pick random from unplayed squares and change data-id to played, and fill square
-  var aiSquare = gameSquares[unplayedIndices[Math.floor(Math.random() * unplayedIndices.length)]];
-  aiSquare.setAttribute('data-id','played');
 
+  if(moveCount >= 4) {
+    for(let itr = 0; itr < unplayedIndices.length; itr++) {
+      checkForWin(gameSquares[unplayedIndices[itr]]);
+      moveValueArray[itr] = moveValue;
+    }   
+  }
+  
+  // store index of the square that represents the best move for the computer
+  var bestIndex = moveValueArray.indexOf(Math.max(...moveValueArray));
+  aiSquare = gameSquares[unplayedIndices[bestIndex]];
+  aiTest = false;
+
+   // select random square for first two rounds
+  if(!aiSquare) {
+    // pick random from unplayed squares and change data-id to played, and fill square
+    aiSquare = gameSquares[unplayedIndices[Math.floor(Math.random() * unplayedIndices.length)]];
+  }
+
+  aiSquare.setAttribute('data-id','played');
   aiSquare.textContent = currentPlayer;
 
-  //setTimeout(function() {aiSquare.style.color = 'white';}, 500);
-  
-  checkForWin(aiSquare);
+  moveCount++;
   currentPlayer = 'X';
   document.getElementById('player-status').textContent = "Your turn!"
-  moveCount++;
+  
 };
 
 var clickSquare = function() {
@@ -109,14 +142,11 @@ var clickSquare = function() {
     
     if(!gameOver) {
       // make the computer take its turn
-      if(currentPlayer === 'X') {
-        currentPlayer = 'O';
-        document.getElementById('player-status').textContent = "AI's turn"
-        aiMove();
-      } else {
-        currentPlayer = 'X';
-        document.getElementById('player-status').textContent = "Your turn!"
-      }
+      currentPlayer = 'O';
+
+      document.getElementById('player-status').textContent = "AI's turn"
+      setTimeout(aiMove, 1000);
+
       document.getElementById('move-count').textContent = "Move: " + moveCount;
     }
   }
